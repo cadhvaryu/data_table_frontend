67cedbf1d836d997b7ee89bcbf21bd68d4e51d7c
chiragadhvaryu <chirag@astrics.io>
2021-03-06 15:42:45 +0530

    Changes In APP file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Template from './components/Template';
 import TemplateRecords from './components/TemplateRecords';
 import TemplateField from './components/TemplateField';
 import TemplateForm from './components/TemplateForm';
+import AllForm from './components/AllForm';
+import SubmitForm from './components/SubmitForm';
 import history from './components/history';
 
 
@@ -12,7 +14,9 @@ class App extends Component {
     return (
      		<Router history={history}>
 				<Switch>
-					<Route exact path="/" component={Template} />	
+					<Route exact path="/" component={Template} />
+					<Route exact path="/forms" component={AllForm} />
+					<Route exact path="/submit-form" component={SubmitForm} />
 					<Route exact path="/template/field/:id" component={TemplateField} />
 					<Route exact path="/template/form/:id" component={TemplateForm} />
 					<Route exact path="/template/records/:id" component={TemplateRecords} />
